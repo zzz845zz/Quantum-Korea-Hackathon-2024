a43260778572fd4b14ec45837e3d96fed9acd61b
Kanguk Lee <p51lee@kaist.ac.kr>
2024-06-21 15:52:07 +0900

    Add .gitignore

---
+++ meta_sabre/meta_sabre_swap.py
@@ -0,0 +1,9 @@
+from qiskit.transpiler.passes.routing.sabre_swap import SabreSwap
+
+
+class MetaSabreSwap(SabreSwap):
+    pass
+
+
+if __name__ == "__main__":
+    pass
--- qkh2024/grader.py
+++ qkh2024/grader.py
@@ -12,14 +12,14 @@ from collections import OrderedDict
 
 class scorer:
     def __init__(
-        self, 
+        self,
     ):
         return
 
     def score(
-        self, 
-        circ: QuantumCircuit, 
-        backend, 
+        self,
+        circ: QuantumCircuit,
+        backend,
     ):
         """
         A custom cost function that includes T1 and T2 computed during idle periods
@@ -38,123 +38,125 @@ class scorer:
         touched = set()
         dt = backend.dt
         num_qubits = backend.num_qubits
-        
+
         t1s = [backend.qubit_properties(qq).t1 for qq in range(num_qubits)]
         t2s = [backend.qubit_properties(qq).t2 for qq in range(num_qubits)]
 
         for item in circ._data:
             for gate in backend.operation_names:
                 if item[0].name == gate:
-                    if (item[0].name == 'cz') or (item[0].name == 'ecr'):
+                    if (item[0].name == "cz") or (item[0].name == "ecr"):
                         q0 = circ.find_bit(item[1][0]).index
                         q1 = circ.find_bit(item[1][1]).index
                         fid *= 1 - backend.target[item[0].name][(q0, q1)].error
                         touched.add(q0)
                         touched.add(q1)
-                    elif item[0].name == 'measure':
+                    elif item[0].name == "measure":
                         q0 = circ.find_bit(item[1][0]).index
-                        fid *= 1 - backend.target[item[0].name][(q0, )].error
+                        fid *= 1 - backend.target[item[0].name][(q0,)].error
                         touched.add(q0)
 
-                    elif item[0].name == 'delay':
+                    elif item[0].name == "delay":
                         q0 = circ.find_bit(item[1][0]).index
                         # Ignore delays that occur before gates
                         # This assumes you are in ground state and errors
                         # do not occur.
                         if q0 in touched:
                             time = item[0].duration * dt
-                            fid *= 1-self._qubit_error(time, t1s[q0], t2s[q0])
+                            fid *= 1 - self._qubit_error(time, t1s[q0], t2s[q0])
                     else:
                         q0 = circ.find_bit(item[1][0]).index
-                        fid *= 1 - backend.target[item[0].name][(q0, )].error
+                        fid *= 1 - backend.target[item[0].name][(q0,)].error
                         touched.add(q0)
 
         return fid
 
     def validate(
-        self, 
-        circuit_target: QuantumCircuit, 
-        circuit_transpiled: QuantumCircuit, 
-        backend, 
+        self,
+        circuit_target: QuantumCircuit,
+        circuit_transpiled: QuantumCircuit,
+        backend,
     ):
         self.grader_operation_check(circuit_transpiled, backend)
         self.grader_connection_check(circuit_transpiled, backend)
         self.grader_circuit_accuracy(circuit_target, circuit_transpiled)
 
     def grader_operation_check(
-        self, 
-        circ: QuantumCircuit, 
-        backend, 
+        self,
+        circ: QuantumCircuit,
+        backend,
     ):
         """
         Takes a backend and a circuit and see whether the gates in the circuit are supported by the backend
 
-        Parameters : 
+        Parameters :
             backend: Target backend
             circuit (QuantumCircuit): Circuit whose gates are to be checked with the gates in the target backend
         """
 
-        op_names_backend : list[str] = backend.operation_names
-        op_names_circuit_dict : dict[str,int]  = circ.count_ops().items()
-        #use set.
+        op_names_backend: list[str] = backend.operation_names
+        op_names_circuit_dict: dict[str, int] = circ.count_ops().items()
+        # use set.
         for keys, _ in op_names_circuit_dict:
             if keys in op_names_backend:
                 None
-            else : 
-                print(Fore.RED + f'The backend does not support {keys} gate')
-        return print(Fore.GREEN + 'Basis gate check passed!')
+            else:
+                print(Fore.RED + f"The backend does not support {keys} gate")
+        return print(Fore.GREEN + "Basis gate check passed!")
 
     def grader_connection_check(
-        self, 
-        circ: QuantumCircuit, 
-        backend, 
+        self,
+        circ: QuantumCircuit,
+        backend,
     ):
-        """"
-        Takes in a Quantum Circuit and a Backend and checks whether the conections in the circuit matches 
-        with the topology of the backend. 
+        """ "
+        Takes in a Quantum Circuit and a Backend and checks whether the conections in the circuit matches
+        with the topology of the backend.
 
-        Parameters: 
+        Parameters:
             backend: Target backend in which the circuit must fit.
             circuit (QuantumCircuit): Circuit whose connections are to be matched with the backend
 
-        Returns: Message: Whether the circuit fits in the topology of the backend. 
+        Returns: Message: Whether the circuit fits in the topology of the backend.
         """
 
         conn_backend = self._connections_backend(backend)
         conn_circ = self._connections_circuit(circ)
 
-        gate_conn_set_backend : dict[str,set] = {}
-        for keys,items in conn_backend.items():
+        gate_conn_set_backend: dict[str, set] = {}
+        for keys, items in conn_backend.items():
             gate_conn_set_backend[keys] = set(items)
 
-        gate_conn_set_circuit : dict[str,set] = {}
-        for keys,items in conn_circ.items():
+        gate_conn_set_circuit: dict[str, set] = {}
+        for keys, items in conn_circ.items():
             gate_conn_set_circuit[keys] = set(items)
 
-        check_connections : list[int] = []
+        check_connections: list[int] = []
         for keys, items in gate_conn_set_circuit.items():
-            check_connections.append(int(gate_conn_set_circuit[keys].issubset(gate_conn_set_backend[keys])))
+            check_connections.append(
+                int(gate_conn_set_circuit[keys].issubset(gate_conn_set_backend[keys]))
+            )
 
         if 0 in check_connections:
-            print(Fore.RED + 'The connections does not match with the backend')
+            print(Fore.RED + "The connections does not match with the backend")
 
-        else : 
-            print(Fore.GREEN + 'The connections match with the backend')
+        else:
+            print(Fore.GREEN + "The connections match with the backend")
 
     def grader_circuit_accuracy(
-        self, 
-        circuit_target: QuantumCircuit, 
-        circuit_transpiled: QuantumCircuit, 
-        simulator = AerSimulator(method="statevector"), 
-        ε: float = 0.75, 
+        self,
+        circuit_target: QuantumCircuit,
+        circuit_transpiled: QuantumCircuit,
+        simulator=AerSimulator(method="statevector"),
+        ε: float = 0.75,
     ):
         """
         Takes the test circuit and compares it with the target circuit.
-        It computes the Hellinger distance and Hellinger Fidelity of the probability 
+        It computes the Hellinger distance and Hellinger Fidelity of the probability
         distribution of the outputs of the to circuits and checks whether they are in
         the tolerance level.
 
-        Parameters : 
+        Parameters :
             circuit_target (QuantumCircuit): Original Quantum Circuit
             circuit_transpiled (QuantumCircuit): Transpiled Circuit.
             simulator: default = AerSimulator
@@ -164,26 +166,36 @@ class scorer:
             Prints out whether the transpiled circuit is within epsilon accuracy of the original circuit.
         """
 
-        circuit_target = transpile(self._remove_idle_qwires(circuit_target), backend = simulator)
-        circuit_transpiled = transpile(self._remove_idle_qwires(circuit_transpiled), backend = simulator)
+        circuit_target = transpile(
+            self._remove_idle_qwires(circuit_target), backend=simulator
+        )
+        circuit_transpiled = transpile(
+            self._remove_idle_qwires(circuit_transpiled), backend=simulator
+        )
 
         counts_target = self._counts(circuit_target)
         counts_transpiled = self._counts(circuit_transpiled)
 
-        hell_dist : float = hellinger_distance(counts_target, counts_transpiled)
-        hell_fid : float = hellinger_fidelity(counts_target, counts_transpiled)
+        hell_dist: float = hellinger_distance(counts_target, counts_transpiled)
+        hell_fid: float = hellinger_fidelity(counts_target, counts_transpiled)
 
-        if hell_fid >= ε or hell_dist <= 1-ε:
-            print(Fore.GREEN + 'Congratualtions! Your circuit is within the given tolerance of the original circuit')
+        if hell_fid >= ε or hell_dist <= 1 - ε:
+            print(
+                Fore.GREEN
+                + "Congratualtions! Your circuit is within the given tolerance of the original circuit"
+            )
 
-        else :
-            print(Fore.RED + 'Oops! Your circuit is not within the given tolerance of the original circuit\n Try again!')
+        else:
+            print(
+                Fore.RED
+                + "Oops! Your circuit is not within the given tolerance of the original circuit\n Try again!"
+            )
 
     def _qubit_error(
-        self, 
-        time: float, 
-        t1: float, 
-        t2: float, 
+        self,
+        time: float,
+        t1: float,
+        t2: float,
     ) -> float:
         """
         Compute the approx. idle error from T1 and T2
@@ -196,36 +208,36 @@ class scorer:
         """
 
         t2 = min(t1, t2)
-        rate1 = 1/t1
-        rate2 = 1/t2
-        p_reset = 1-np.exp(-time*rate1)
-        p_z = (1-p_reset)*(1-np.exp(-time*(rate2-rate1)))/2
+        rate1 = 1 / t1
+        rate2 = 1 / t2
+        p_reset = 1 - np.exp(-time * rate1)
+        p_z = (1 - p_reset) * (1 - np.exp(-time * (rate2 - rate1))) / 2
         return p_z + p_reset
 
     def _connections_backend(
-        self, 
-        backend, 
-    ) -> dict[str, list]: 
+        self,
+        backend,
+    ) -> dict[str, list]:
         """
         Takes a backend and returns the connections.
 
-        Parameters : 
+        Parameters :
             Inputs a backend
 
-        Returns: 
+        Returns:
             The connection of the gates as a dictionary,
             the keys as gates and values as connections
         """
 
         instruc: list = backend.instructions
         operations: list = backend.operations
-        oper_key : list[str] = []
+        oper_key: list[str] = []
 
         for items in operations:
-            if isinstance(items, Instruction) and items.num_clbits == 0 :
+            if isinstance(items, Instruction) and items.num_clbits == 0:
                 oper_key.append(items.name)
 
-        list_dict : dict[str, list] = {name : [] for name in oper_key}
+        list_dict: dict[str, list] = {name: [] for name in oper_key}
         for gates in range(len(oper_key)):
             for i in range(len(instruc)):
                 if instruc[i][0].name == oper_key[gates]:
@@ -234,17 +246,14 @@ class scorer:
 
         return list_dict
 
-    def _connections_circuit(
-        self, 
-        circ : QuantumCircuit
-    ) -> dict[str,list]: 
+    def _connections_circuit(self, circ: QuantumCircuit) -> dict[str, list]:
         """
         Takes a Quantum Circuit and returns the connections.
 
-        Parameters : 
+        Parameters :
             Inputs a QuantumCircuit
 
-        Returns: 
+        Returns:
             The connection of the gates as a dictionary,
             the keys as gates and values as connections
         """
@@ -252,45 +261,45 @@ class scorer:
         gates_used: list = []
         for keys, _ in circ.count_ops().items():
             gates_used.append(keys)
-        list_dict : dict[str,list] = {name : [] for name in gates_used}
-        for gates in gates_used: 
+        list_dict: dict[str, list] = {name: [] for name in gates_used}
+        for gates in gates_used:
             for i in range(len(circ.data)):
                 if circ.data[i].operation.name == gates:
                     qubit_tuple = ()
                     for j in range(len(circ.data[i].qubits)):
                         qubit_index = circ.data[i].qubits[j]._index
                         qubit_tuple = qubit_tuple + (qubit_index,)
-                    list_dict[gates].append(qubit_tuple)   
+                    list_dict[gates].append(qubit_tuple)
         return list_dict
 
     def _counts(
-        self, 
-        circ : QuantumCircuit, 
-        simulator = AerSimulator(method="statevector"), 
+        self,
+        circ: QuantumCircuit,
+        simulator=AerSimulator(method="statevector"),
     ) -> dict[str, int]:
         """
         Function to add measurements to a provided circuit and run it on the AerSimulator.
 
-        Parameters 
+        Parameters
             circuit (QuantumCircuit)
             simulator: default = AerSimulator
 
-        Returns  
+        Returns
             counts in a dict.
         """
 
-        if 'measure' in circ.count_ops():
+        if "measure" in circ.count_ops():
             None
-        else : 
+        else:
             circ.measure_active()
-        result = simulator.run(circ, backend = simulator).result()
+        result = simulator.run(circ, backend=simulator).result()
         counts = result.get_counts()
 
         return counts
 
     def _remove_idle_qwires(
-        self, 
-        circ: QuantumCircuit, 
+        self,
+        circ: QuantumCircuit,
     ):
         dag = circuit_to_dag(circ)
 
